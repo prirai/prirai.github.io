7224bfd5d7a25d6870bb1c6508952bc516747181
Priyanshu Rai <idealpriyanshu@gmail.com>
2023-05-15 20:42:00 +0530

    Search page and other improvements

--- .eleventy.js
+++ .eleventy.js
@@ -11,6 +11,7 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/cdn");
     eleventyConfig.addPassthroughCopy("src/assets/fonts");
     eleventyConfig.addPassthroughCopy("src/assets/js");
+    eleventyConfig.addPassthroughCopy("src/assets/img");
     eleventyConfig.addTemplateFormats("scss");
 
     eleventyConfig.setBrowserSyncConfig({
